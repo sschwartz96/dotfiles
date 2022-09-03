15798deba3b9f5b51bfc05af0b06d9d754c5c283
sam schwartz <sam.schwartz96@gmail.com>
2022-09-03 12:28:31 -0500

    desktop updates

--- scripts/rclone-sync.sh
+++ scripts/rclone-sync.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Sync from google cloud first
-rclone sync -P schwartzit: /home/sam/schwartzit
+rclone sync -P --drive-skip-gdocs schwartzit: /home/sam/schwartzit
 echo "Sync from Google Drive complete."
 
 while true
 do
 	# Once per minute copy changes to the cloud
 	sleep 1m
-	rclone sync -P /home/sam/schwartzit schwartzit:
+	rclone sync -P --drive-skip-gdocs /home/sam/schwartzit schwartzit:
 	echo "Sync to Google Drive complete."
 done
