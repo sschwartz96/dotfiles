673096e3ee1ad9191eaceffd47c28456b045a32b
Sam Schwartz <sam.schwartz96@gmail.com>
2022-10-16 20:38:14 -0500

    changes formatting_snyc to format and adds nvim-surround and aerial plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -36,6 +36,12 @@ return require('packer').startup(function(use)
   -- debugging
   use 'mfussenegger/nvim-dap'
 
+  -- surround
+  use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+  })
+
 
   ------------------------------- Visuals ---------------------------------
 
@@ -53,6 +59,9 @@ return require('packer').startup(function(use)
 
   -- file tree
   use { 'kyazdani42/nvim-tree.lua', tag = 'nightly' }
+
+  -- outline
+  use { 'stevearc/aerial.nvim' }
 
 
   ---------------------------- Language Support ----------------------------
@@ -93,4 +102,22 @@ return require('packer').startup(function(use)
 
   -- nvim-autopairs
   require("nvim-autopairs").setup({})
+
+  -- aerial outline
+  require('aerial').setup({
+    on_attach = function(bufnr)
+      -- Toggle the aerial window with <leader>a
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>a', '<cmd>AerialToggle!<CR>', {})
+      -- Jump forwards/backwards with '{' and '}'
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', '{', '<cmd>AerialPrev<CR>', {})
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', '}', '<cmd>AerialNext<CR>', {})
+      -- Jump up the tree with '[[' or ']]'
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', '[[', '<cmd>AerialPrevUp<CR>', {})
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', ']]', '<cmd>AerialNextUp<CR>', {})
+    end
+  })
+
+  -- surround
+  require("nvim-surround").setup({})
+
 end)
