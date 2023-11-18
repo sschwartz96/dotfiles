0be167c545621d1f90b284cea671dbf96eb21c30
Sam Schwartz <sam.schwartz96@gmail.com>
2023-11-17 22:42:00 -0600

    simplify update-tools.sh

--- update-tools.sh
+++ update-tools.sh
@@ -2,10 +2,22 @@
 
 # requires pnpm and go
 
-pnpm i -g yaml-language-server
-pnpm i -g svelte-language-server
-pnpm i -g @tailwindcss/language-server
-pnpm i -g typescript-language-server
-pnpm i -g vscode-langservers-extracted
+# list node packages to install
 
-GO111MODULE=on go install golang.org/x/tools/gopls@latest
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+node_packages=("pnpm" "yaml-language-server" "svelte-language-server" "@tailwindcss/language-server" "typescript-language-server" "vscode-langservers-extracted")
+
+for p in ${node_packages[@]}; do
+	echo -e "    ${GREEN}Updating $p ${NC}"
+	pnpm i -g $p
+	echo ""
+done
+
+go_packages=("golang.org/x/tools/gopls@latest" "github.com/go-delve/delve/cmd/dlv@latest")
+
+for p in ${go_packages[@]}; do
+	echo -e "    ${GREEN}Updating $p ${NC}"
+	GO111MODULE=on go install $p
+done
