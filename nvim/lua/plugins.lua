6862c1518efa5cc2ba407bdc683e149fd77f88a9
Sam Schwartz <sam.schwartz96@gmail.com>
2024-02-08 03:51:52 -0600

    adds flutter support for debugging

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -145,6 +145,16 @@ require("lazy").setup({
   -- dart
   'dart-lang/dart-vim-plugin',
 
+  -- flutter
+  {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+  },
 
 })
 
@@ -215,4 +225,22 @@ require('fzf-lua').setup({
   files = {
     cmd = vim.env.FZF_DEFAULT_COMMAND
   }
+})
+
+require('flutter-tools').setup({
+  widget_guides = {
+    enabled = true,
+  },
+  debugger = {          -- integrate with nvim dap + install dart code debugger
+    enabled = true,
+    run_via_dap = true, -- use dap instead of a plenary job to run flutter apps
+    -- if empty dap will not stop on any exceptions, otherwise it will stop on those specified
+    -- see |:help dap.set_exception_breakpoints()| for more info
+    exception_breakpoints = {},
+    register_configurations = function(_)
+      local dap = require("dap")
+      dap.configurations.dart = {}
+      require("dap.ext.vscode").load_launchjs()
+    end,
+  },
 })
