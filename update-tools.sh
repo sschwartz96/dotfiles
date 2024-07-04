b46ad6437616c13a6a115ee980eca32e704283bf
sam schwartz <sam.schwartz96@gmail.com>
2024-07-04 17:37:50 -0500

    added crates to the update-tools.sh script

--- update-tools.sh
+++ update-tools.sh
@@ -14,9 +14,17 @@ for p in ${node_packages[@]}; do
 	echo ""
 done
 
-go_packages=("golang.org/x/tools/gopls@latest" "github.com/go-delve/delve/cmd/dlv@latest")
+go_packages=("golang.org/x/tools/gopls@latest" "github.com/go-delve/delve/cmd/dlv@latest" "github.com/nametake/golangci-lint-langserver@latest")
 
 for p in ${go_packages[@]}; do
 	echo -e "    ${GREEN}Updating $p ${NC}"
 	GO111MODULE=on go install $p
+done
+
+# TODO : add rust???
+cargo_crates=("$ https://github.com/estin/simple-completion-language-server.git")
+
+for c in ${cargo_crates[@]}; do
+	echo -e "    ${GREEN}Updating $c ${NC}"
+	cargo install --git $c
 done
